112167f67daeacb0125a450a1f28b9c57136252f
AnastasiyaValushko <nastya.valushko@yandex.ru>
2022-08-08 16:49:48 +0300

    add switch in task_10

--- JS/js_task_10.js
+++ JS/js_task_10.js
@@ -12,3 +12,31 @@ let month = (day <= 31) ? ('Январь'):
         (day <= 365) ? ('Декабрь') : ('Такого месяца не существует');
 console.log(month);  
 
+let season = "";
+
+switch (month) {
+  case "Декабрь":
+  case "Январь":
+  case "Февраль":
+    season = "Зима";
+    break;
+  case "Март":
+  case "Апрель":
+  case "Май":
+    season = "Весна";
+    break;
+  case "Июнь":
+  case "Июль":
+  case "Август":
+    season = "Лето";
+    break;
+  case "Сентябрь":
+  case "Октябрь":
+  case "Ноябрь":
+    season = "Осень";
+    break;
+  default:
+    season = "Нет такой поры года!";
+}
+
+alert(season);
