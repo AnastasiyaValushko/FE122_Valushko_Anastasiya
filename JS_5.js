3977b274cb932b9e3f82220292d94c5bd34de11a
AnastasiyaValushko <nastya.valushko@yandex.ru>
2022-08-18 21:33:55 +0300

    task13

--- JS_5.js
+++ JS_5.js
@@ -97,23 +97,22 @@ console.log('Task7--------');
 // };
 // createPyramid(prompt('число'));
 
-function createPyramid(height){
-    i = 0,
-    j = 0;
-    let empty = "",
-        star = "";
-
-    while (i < height) {
-    i++;
-    empty = "";
-    star = "";
-        for (j = 0; j < height - 1; j++) empty += " ";
-        for (j = 0; j < 2 * i + 1; j++) star += height;
-        console.log(empty + star);
+function createPyramid(num, symbol){
+    let arr = [];
+    for (let i = 1; i <= num; i++){
+        let n = '';
+        for (let j = 0; j < i; j++){
+            if (symbol){
+                n += symbol;
+            }else{
+                n += i;
+            }
+        }
+        arr.push(n);
     }
+    return arr;
 }
-createPyramid(5);
-
+console.log(createPyramid());
 
 //task8
 console.log('Task8--------');
@@ -136,20 +135,133 @@ function createArray3(a){
 let result3 = createArray3(16);
 console.log(result3);
 
+// function fib(num){
+//     return num <= 1 ? num : fib(num - 1) + fib(num - 2);
+// }
+
+
 //task10
 console.log('Task10--------');
 
-let num = 16;
-function printSum(a){
-    var arr = a.split('');
-    var sum = 0;
-    for(var i = 0; i < arr.length; i++){
-        sum += arr[i];
+// let num = 16;
+// function printSum(a){
+//     var arr = a.split('');
+//     var sum = 0;
+//     for(var i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//     if(sum > 9){
+//         printSum(sum + '');
+//     }else{
+//         console.log(sum);
+//     }
+// }
+// printSum(num);
+
+
+//task12
+console.log('Task12--------');
+
+function t12(name, surname, lastName, num){
+    let title = 'Домашняя работа: "Функции"';
+    let subTitle = `Выполнил: студент гр. ${num}`;
+    let nameText = name + ' ' + surname + ' '+ lastName; // `${name} ${surname} ${lastName}`;
+
+    let maxStr = 0;
+    if(title.length > maxStr) maxStr = title.length;
+    if(subTitle.length > maxStr) maxStr = subTitle.length;
+    if(nameText.length > maxStr) maxStr = nameText.length;
+
+    title = makeString(title, maxStr);
+    subTitle = makeString(subTitle, maxStr);
+    nameText = makeString(nameText, maxStr);
+
+    let ramka = '*';
+
+    for(let i = 0; i < maxStr + 3; i++){
+        ramka += '*';
     }
-    if(sum > 9){
-        printSum(sum + '');
-    }else{
-        console.log(sum);
-    }
+    console.log(ramka + '\n' + title + '\n' + subTitle + '\n' + nameText + '\n' + ramka);
+
 }
-printSum(num);
+t12('Nastya', 'Valushka', 'Viktorovna', 'FE122');
+
+function makeString(str, l){
+    for(let i = str.length; i < l; i++){
+        str += ' ';
+    }
+
+    str = '* ' + str + ' *';
+    return str;
+}
+
+//task13
+console.log('Task13--------');
+
+function t13(str){
+    let point = str.lastIndexOf('.');
+    if(point === str.length - 1 || point === str.length - 2 || point === 0) return 'Не почтовый адрес';
+
+    let dog = str.lastIndexOf('@');
+    if(dog !== str.lastIndexOf('@')) return 'Не почтовый адрес';
+    if(dog === str.length - 1 || dog === str.length - 2 || dog === 0 || dog < 3) return 'Не почтовый адрес';
+
+    let defis = str.lastIndexOf('-');
+    if(defis === str.length - 1 || defis === 0) return 'Не почтовый адрес';
+
+    let n = str.lastIndexOf('_');
+    if(n === str.length - 1 || n === 0) return 'Не почтовый адрес';
+
+    let symbols = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ!\"\'#$%&()*+,/:;<=>?[|}{'
+
+    let nums = '1234567890';
+
+    let nameSymbols = '!\"\'#$%&()*+,/:;<=>?[|}{-@_';
+
+    for(let i = 0; i < str.length - 1; i++){
+        if (str[i] === '.' && str[i + 1] === '.' ||
+        str[i] === '@' && srt[i + 1] === '.' ||
+        str[i] === '@' && srt[i + 1] === '@' ||
+        str[i] === '@' && srt[i + 1] === '_' ||
+        str[i] === '@' && srt[i + 1] === '-' ||
+
+        str[i] === '_' && srt[i + 1] === '.' ||
+        str[i] === '_' && srt[i + 1] === '_' ||
+        str[i] === '_' && srt[i + 1] === '-' ||
+        str[i] === '_' && srt[i + 1] === '@' ||
+        
+        str[i] === '-' && srt[i + 1] === '.' ||
+        str[i] === '-' && srt[i + 1] === '_' ||
+        str[i] === '-' && srt[i + 1] === '-' ||
+        str[i] === '-' && srt[i + 1] === '@' ||
+
+        str[i] === '.' && srt[i + 1] === '.' ||
+        str[i] === '.' && srt[i + 1] === '_' ||
+        str[i] === '.' && srt[i + 1] === '-' ||
+        str[i] === '.' && srt[i + 1] === '@') return 'Не почтовый адрес';
+
+        if(symbols.includes(str[i])) return 'Не почтовый адрес';
+    }
+
+    let postName = str.slice(0, dog);
+
+    if(nums.includes(postName[0])) return 'Не почтовый адрес';
+
+    for (let i = 0; i < postName.length; i++){
+        if(nameSymbols.includes(postName[i])) return 'Не почтовый адрес';
+    }
+
+    let dom = str.slice(point + 1);
+    let domName = str.slice(dog, point);
+
+    if(dom.length < 2 || dom.length > 11) return 'Не почтовый адрес';
+
+    for(let i = 0; i < dom.length; i++){
+        if(nums.includes(dom[i])) return 'Не почтовый адрес';
+    }
+
+    if(domName.length < 2 || domName.length > 11) return 'Не почтовый адрес';
+
+
+    return 'Почтовый адрес верен';
+}
