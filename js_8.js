6841854d61d4c9a8b6596e77a339d31a2d541c93
AnastasiyaValushko <nastya.valushko@yandex.ru>
2022-08-29 11:55:25 +0300

    Update js_8

--- js_8.js
+++ js_8.js
@@ -19,6 +19,7 @@ document.body.appendChild(p1);
 
 let divDisplay = document.createElement('div');
 divDisplay.innerHTML = '';
+divDisplay.classList.add('divDisplay');
 document.body.appendChild(divDisplay);
 
 let div11 = document.createElement('div');
@@ -36,52 +37,69 @@ h5.innerHTML = 'FREELANCER';
 div1.appendChild(h5);
 
 let h2 = document.createElement('h2');
-h2.innerHTML = 'Initially designed to ';
+h2.innerHTML = 'Initially</br> designed to ';
 div1.appendChild(h2);
 
-p1 = document.createElement('p');
-p1.innerHTML = 'But I must explain to you how all this mistaken idea of denouncing';
-p1.classList.add('p1');
-div1.appendChild(p1);
+p2 = document.createElement('p');
+p2.innerHTML = 'But I must explain to you how all this mistaken idea of denouncing';
+p2.classList.add('p2');
+div1.appendChild(p2);
 
-let button = document.createElement('p');
+let button = document.createElement('button');
 button.innerHTML = 'START HERE';
 div1.appendChild(button);
 
-
-
-
-
-
-
-
-
-
-
+let clone = div11.cloneNode(true);
+clone.classList.add('clone');
+clone.querySelector('h5').innerHTML = 'STUDIO';
+divDisplay.appendChild(clone);
 
 
 let style = document.createElement('style');
 style.innerHTML = `
+    @import url('https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat:wght@700&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&display=swap');
+
+
     body{
         text-align: center;
     }
 
+    .clone{
+        background-color: #8F75BE;
+    }
+    .clone h2,.clone p, .clone button {
+        color: white;
+    }
+    .clone button{
+        background-color: #8F75BE;
+    }
+
+    .clone h5{
+        color: #FFC80A;
+        
+    }
+
+    .divDisplay{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 55px;
+    }
+
     .div11{
         border: 1px solid #E8E9ED;
-        width: 400px;
+        width: 260px;
         height: 479px;
         display: flex;
         align-items: center;
+        justify-content: center;
+        padding:  0 70px;
     }
 
-
-    .div1{
-        
-    }
     
     h1 {
         text-align: center;
-        font-family: 'Arvo';
+        font-family: 'Arvo', serif;
         font-style: normal;
         font-weight: 400;
         font-size: 36px;
@@ -90,14 +108,21 @@ style.innerHTML = `
     }
 
     .p1{
-        font-family: 'OpenSans';
+        font-family: 'Open Sans', sans-serif;
         font-size: 14px;
         line-height: 26px;
         color: #9FA3A7;
     }
 
+    .p2{
+        font-family: 'Open Sans', sans-serif;
+        font-size: 12px;
+        line-height: 22px;
+        color: #9FA3A7;
+    }
+
     h5 {
-        font-family: 'Montserrat';
+        font-family: 'Montserrat', sans-serif;
         font-style: normal;
         font-weight: 700;
         font-size: 12px;
@@ -114,6 +139,21 @@ style.innerHTML = `
         line-height: 46px;
         color: #212121;
         }
+
+    button{    
+        width: 147px;
+        height: 46px;
+        border: 3px solid #FFC80A;
+        border-radius: 25px;
+        background-color: #fff;
+        font-family: 'Montserrat', sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 12px;
+        letter-spacing: 2.4px;
+        color: #212121;
+        margin-top: 60px;
+    }
 
 
 `;
