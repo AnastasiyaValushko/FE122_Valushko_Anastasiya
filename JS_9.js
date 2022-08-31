1116c1a67e5b4a101b4cb56fcdfdd8fac3a65081
AnastasiyaValushko <nastya.valushko@yandex.ru>
2022-08-31 21:21:16 +0300

    update

--- JS_9.js
+++ JS_9.js
@@ -1,20 +1,43 @@
 window.addEventListener('load', function(){
     console.log('Страница загружена')
 
+    let checkbox = document.querySelector('.checkbox');
+    
+    // Array.from(checkbox).forEach(function(e){
+    //     e.addEventListener('click', function(e){
+    //         e.target.value = 'FOCUS';
+    //     });
+    //     e.addEventListener('blur', function(e){
+    //         e.target.value = '';
+    //     });
+    // });
+
+    
+    checkbox.addEventListener('mousedown', function(){
+        checkbox.style.backgroundColor = 'rgb(226, 182, 169)';
+    });
+
+    let checked = document.querySelector('li');
+    
+
     let input = document.querySelector('input.list-text');
     let ul = document.querySelector('ul.todos');
 
+
     function createToDo(){
+
+        
         let li = document.createElement('li');
         let text = document.createElement('span');
         text.classList.add('todo-text');
         let newToDo = input.value;
         text.append(newToDo);
 
+        
         let deleteButton = document.createElement('span');
         deleteButton.classList.add('trash');
-        let icon = document.createElement('img');
-        icon.classList.add('trash');
+        let icon = document.createElement('i');
+        icon.classList.add('fa', 'fa-trash', 'fa-2x');
         deleteButton.appendChild(icon);
 
         ul.appendChild(li).append(text, deleteButton);
@@ -30,11 +53,11 @@ window.addEventListener('load', function(){
     }
 
     function onClickToDo(event){
-        if (event.target.tagName === 'Li'){
+        if (event.target.tagName === 'li'){
             event.target.classList.toggle('checked');
         }
     }
-    
+  
 
 
     input.addEventListener("keypress", (keyPressed) => {
