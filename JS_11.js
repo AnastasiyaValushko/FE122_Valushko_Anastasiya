3c78724e61d83f1bebddbe084059a0a32180939f
AnastasiyaValushko <nastya.valushko@yandex.ru>
2022-09-27 21:13:51 +0300

    add contacts

--- JS_11.js
+++ JS_11.js
@@ -1,57 +1,128 @@
-let input1 = document.getElementById("input1");
-let input2 = document.getElementById("input2");
-let submit = document.getElementById("submit");
-let id = document.getElementById("id");
-let name = document.getElementById("name");
-let email = document.getElementById("email");
-let address = document.getElementById("address");
-let phone = document.getElementById("phone");
-
-// submit.addEventListener("click", function() {
-
-// })
-
-class Contacts {
-    constructor(data){
-        this.user = [];
-    }
-
-    add(contacts) {
-        this.user.push(contacts);
-    }
-
-    get user1() {
-        return this.user;
-    }
-
-    set user1(user1) {
-        this.user = user1;
-    }
-
-    edit(id,obj){
-        
-    }
-    
-}
-
 class User {
-    constructor(data){
+    #data
+    constructor(data) {
         this.id = data.id;
-        this.name = data.name;
-        this.email = data.email;
-        this.address = data.address;
-        this.phone = data.phone;
+        this.name = data.name,
+        this.email = data.email,
+        this.address = data.address,
+        this.phone = data.phone        
     }
 
-    set edit(obj) {
-        this.data = obj; 
+    edit(data){
+        Object.assign(this.#data, data);
     }
 
-    get info() {
-        return this.name + ' (' + this.email + ', ' + this.address + ', ' + this.phone + ')';
+    get data(){
+        return this.#data;
+    }
+
+    set data(data){
+        Object.assign(this.#data, data);
     }
 }
 
-let alex = new User( { id: '0', name:'Alex', email: 'alex@gmail.com', address: 'Minsk, Nemanskaya street', phone: '6464541'} );
-console.log(alex);
-console.log(alex.info);
+class Contacts{
+    #contacts;
+    constructor(data){
+        this.data = [];
+    }
+
+    add(obj){
+        if(!obj.title) return;
+
+        let contact = new User(obj);
+        let id = this.getRandomId();
+        contact.data = {id};
+        this.#contacts.push(contact);
+        return this;
+    }
+
+    getRandomId(){
+        let id = Math.floor(Math.random() * 1000000);
+        if(this.#contacts.length === 0) return id;
+
+        let flag = this.#contacts.some(contact => contact.data.id === id);
+        if(flag){
+            return this.getRandomId();
+        }else{
+            return id;
+        }
+    }
+
+    edit(id, data){
+        this.#contacts.forEach(contact =>{
+            if (contact.data.id === id){
+                contact.data = data;
+            }
+        });
+        return this;
+    }
+
+    remove(id){
+        this.#contacts = this.#contacts.filter(contact => contact.data.id !== id);
+        return this;
+    }
+}
+
+class ContactsApp extends Contacts{
+    constructor(selector){
+        super();
+        this.app = null;
+        this.init(selector);
+    }
+
+    init(selector){
+        this.app = document.querySelector('body');
+        let contactsContainer = this.createElement('div', [
+            ['class', 'contacts']
+        ]);
+        let formContainer = this.createElement('form');
+        let nameInput = this.createElement('input', [
+            ['type', 'text'],
+            ['placeholder','Введите Ваше имя']
+        ]);
+        let emailInput = this.createElement('input', [
+            ['type', 'text'],
+            ['placeholder','Введите Вашу электронную почту']
+        ]);
+        let addressInput = this.createElement('input', [
+            ['type', 'text'],
+            ['placeholder','Введите Ваш адрес']
+        ]);
+        let phoneInput = this.createElement('input', [
+            ['type', 'text'],
+            ['placeholder','Введите Ваш номер телефона']
+        ]);
+
+        let addButton = this.createElement('button', [
+            ['type', 'submit']
+        ], 'Добавить контакт');
+
+        contactsContainer.append(formContainer);
+
+        formContainer.append(nameInput, emailInput, addressInput, phoneInput, addButton);
+
+        this.app.appendChild(contactsContainer);
+
+    }
+
+    
+
+    createElement(name, attributes = [], content = ''){
+        let elem = document.createElement(name);
+
+        if(attributes.length > 0){
+            attributes.forEach(attr => elem.setAttribute(attr[0], attr[1]));
+        }
+
+        if (content){
+            elem.innerHTML = content;
+        }
+        return elem;
+    };
+}
+
+new ContactsApp('.contacts')
+
+
+ 
