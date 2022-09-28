acc73b5b12cd1b30d5670a8302bb032afac4f87e
AnastasiyaValushko <nastya.valushko@yandex.ru>
2022-09-28 21:47:56 +0300

    update

--- JS_11.js
+++ JS_11.js
@@ -1,14 +1,11 @@
 class User {
-    #data
+    #data;
     constructor(data) {
-        this.id = data.id;
-        this.name = data.name,
-        this.email = data.email,
-        this.address = data.address,
-        this.phone = data.phone        
+        if(data.name.length > 0) this.#data = data;   
     }
 
     edit(data){
+        
         Object.assign(this.#data, data);
     }
 
@@ -17,18 +14,20 @@ class User {
     }
 
     set data(data){
+        console.log(this.#data);
+        console.log(data);
         Object.assign(this.#data, data);
     }
 }
 
 class Contacts{
     #contacts;
-    constructor(data){
-        this.data = [];
+    constructor(){
+        this.#contacts = [];
     }
 
     add(obj){
-        if(!obj.title) return;
+        if(!obj.name || !obj.email || !obj.address || !obj.phone) return;
 
         let contact = new User(obj);
         let id = this.getRandomId();
@@ -62,21 +61,27 @@ class Contacts{
         this.#contacts = this.#contacts.filter(contact => contact.data.id !== id);
         return this;
     }
+
+    get contacts(){
+        return this.#contacts;
+    }
 }
 
 class ContactsApp extends Contacts{
     constructor(selector){
         super();
         this.app = null;
+        this.contactsContainer2 = null;
         this.init(selector);
     }
 
-    init(selector){
+    init(){
         this.app = document.querySelector('body');
         let contactsContainer = this.createElement('div', [
             ['class', 'contacts']
         ]);
         let formContainer = this.createElement('form');
+        
         let nameInput = this.createElement('input', [
             ['type', 'text'],
             ['placeholder','Введите Ваше имя']
@@ -97,16 +102,101 @@ class ContactsApp extends Contacts{
         let addButton = this.createElement('button', [
             ['type', 'submit']
         ], 'Добавить контакт');
+        
+        formContainer.append(nameInput, emailInput, addressInput, phoneInput, addButton);  
+        formContainer.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let data = {
+                id: 0,
+                name: nameInput.value,
+                email: emailInput.value,
+                address: addressInput.value,
+                phone: phoneInput.value,
+            };
 
-        contactsContainer.append(formContainer);
+            this.add(data);
+            this.onAdd();
+            nameInput.value = '';
+            emailInput.value = '';
+            addressInput.value = '';
+            phoneInput.value = '';
+        });
+        
 
-        formContainer.append(nameInput, emailInput, addressInput, phoneInput, addButton);
+        this.contactsContainer2 = this.createElement('div', [
+            ['class', 'contacts2']
+        ]);
+            
+        contactsContainer.append(formContainer, this.contactsContainer2);
+         
 
-        this.app.appendChild(contactsContainer);
+        this.app.append(contactsContainer);
 
     }
 
-    
+    onAdd(){
+        this.contactsContainer2.innerHTML = '';
+        this.contacts.forEach(contact => {
+            let contactElem = this.createElement('div', [
+                ['class', 'contact']
+            ]);
+            
+            let contactName = this.createElement('h2', [
+                ['class', 'name']
+            ], contact.data.name);
+
+            let contactEmail = this.createElement('p', [
+                ['class', 'email']
+            ], contact.data.email);
+
+            let contactAddress = this.createElement('p', [
+                ['class', 'address']
+            ], contact.data.address);
+
+            let contactPhone = this.createElement('p', [
+                ['class', 'phone']
+            ], contact.data.phone);
+
+            let onEdit = this.createElement('button', [], 'Edit');
+            let onRemove = this.createElement('button', [], 'Remove');
+            let flag = true;
+
+            onEdit.addEventListener('click', () => {
+                if (flag){
+                    contactName.contentEditable = true;
+                    contactEmail.contentEditable = true;
+                    contactAddress.contentEditable = true;
+                    contactPhone.contentEditable = true;
+                    onEdit.innerHTML = 'Save';
+                    flag = !flag;
+                }else{
+                    contactName.contentEditable = false;
+                    contactEmail.contentEditable = false;
+                    contactAddress.contentEditable = false;
+                    contactPhone.contentEditable = false;
+                    onEdit.innerHTML = 'Edit';
+                    flag = !flag;
+
+                    let data = {
+                        name: contactName.innerText,
+                        email: contactEmail.innerText,
+                        address: contactAddress.innerText,
+                        phone: contactPhone.innerText,
+                    }
+                    this.onEdit(contact.data.id, data);
+                    console.log(this.contacts);
+                }
+            });
+
+            onRemove.addEventListener('click', () => {
+                this.remove(contact.data.id);
+                this.onAdd();
+            });         
+
+            contactElem.append(contactName,contactEmail,contactAddress, contactPhone, onEdit, onRemove);
+            this.contactsContainer2.append(contactElem);
+        });
+    }
 
     createElement(name, attributes = [], content = ''){
         let elem = document.createElement(name);
